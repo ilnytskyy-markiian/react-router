625ab98ef4b751b0a1e1f1158ccce4a561b5affb
Ilnytskyy Markiian <ilnytskyymarkiian@gmail.com>
2026-04-02 21:51:36 +0300

    feat: create SPA with error page

---
+++ src/error-page.jsx
@@ -0,0 +1,18 @@
+import { useRouteError } from "react-router-dom";
+
+export default function ErrorPage() {
+  const error = useRouteError();
+  console.error(error.message);
+
+  return (
+    <>
+      <div id="error-page">
+        <h1>Oops!</h1>
+        <p>Sorry, an unexpected error has occured.</p>
+        <p>
+          <i>{error.statusError || error.message}</i>
+        </p>
+      </div>
+    </>
+  );
+}
