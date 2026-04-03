25dc1229ebb06c808ead4534fb432d4a7744815c
Ilnytskyy Markiian <ilnytskyymarkiian@gmail.com>
2026-04-03 13:12:43 +0300

    fix: add dynamic contact fetching using loader

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
-import Root from "./routes/root.jsx";
+import Root, { loader as rootLoader } from "./routes/root.jsx";
 import ErrorPage from "./error-page.jsx";
 import Contact from "./routes/contacts.jsx";
 import './index.css';
@@ -14,6 +14,7 @@ const router = createBrowserRouter([
     path: "/",
     element: <Root />,
     errorElement: <ErrorPage />,
+    loader: rootLoader,
     children: [
       {
         path: "/contacts/:contactId",
